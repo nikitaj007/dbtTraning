67e03ae44eb765b7e03f483365c47cc3e079ad4d
nikitaj007@gmail.com <nikitaj007@gmail.com>
2025-03-10 08:39:58 +0000

    developing stg_orders

---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,27 @@
+{{
+    config(
+        materialized='table'
+    )
+}}
+
+
+select 
+o.ORDERID ,
+o.ORDERDATE,
+o.SHIPDATE,
+o.SHIPMODE,
+o.ORDERSELLINGPRICE - o.ORDERCOSTPRICE as orderprofit,
+
+c.CUSTOMERNAME,
+c.SEGMENT,
+c.COUNTRY,
+
+p.CATEGORY,
+p.PRODUCTNAME,
+p.SUBCATEGORY
+
+from {{ ref('raw_order') }} as o
+left join {{ ref('raw_customer') }} as c on c.CUSTOMERID=o.CUSTOMERID
+left join {{ ref('raw_product') }} as p on p.PRODUCTID=o.PRODUCTID
+
+
